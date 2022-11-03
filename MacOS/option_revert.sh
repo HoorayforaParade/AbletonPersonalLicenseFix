08d1976e75db6e07763bdcb27af2f793eb4a4111
Neil Andruski <neil.andruski@gmail.com>
2022-11-03 11:19:26 -0400

    Added MacOS shell scripts
    Added MacOS shell scripts for removing the -licenseserver option and to revert it.

---
+++ MacOS/option_revert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is a bash script for testing out my license removal script. Used to revert the back to being the primary.
+# Original script makes a backup of the options file then edits to remove the -licenseserver line case insensitive
+
+#Directory path to Ableton preferences folder. Where each version has their own directory
+path="/Users/$USER/Library/Preferences/Ableton"
+
+#Loop through the Ableton Live version folders. Find the Options file and replaces the license server line.
+for dir in "$path"/* ; do
+	[ -d "$dir" ] || continue
+	#echo "$dir"/Options.txt		#Debug File Path
+
+	# Remove Options file without -licenseserver then rename .bak to be Options.txt
+	rm "$dir"/Options.txt
+	mv "$dir"/Options.txt.bak "$dir"/Options.txt 
+done

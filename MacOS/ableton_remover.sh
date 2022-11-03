4fabb07d6cf8919705d096c2ce956ab17e94f828
Neil Andruski <neil.andruski@gmail.com>
2022-11-03 11:21:16 -0400

    Update ableton_remover.sh
    Removed unnecessary testing commands.

--- MacOS/ableton_remover.sh
+++ MacOS/ableton_remover.sh
@@ -4,13 +4,11 @@
 # This will allow users to use their own license with Ableton without deleting everything nor look for the file themselves.
 
 #Directory path to Ableton preferences folder. Where each version has their own directory
-#path="/Users/$USER/Documents/Work/ableton_remover/"
 path="/Users/$USER/Library/Preferences/Ableton"
 
 #Loop through the Ableton Live version folders. Find the Options file and replaces the license server line.
 for dir in "$path"/* ; do
 	[ -d "$dir" ] || continue
-	echo "$dir"/Options.txt		#Debug File Path
 
 	#Change the license server line, Save original with .bak postfix to roll back if user needs to.
 	sed -i '.bak' -r -e 's/-[Ll]icenseserver//g' "$dir"/Options.txt

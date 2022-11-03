7dd8d498afdbc00e9112ddf6378d2c795268c32f
Neil Andruski <neil.andruski@gmail.com>
2022-11-03 15:36:28 -0400

    Add files via upload
    Started Windows functionality

---
+++ Windows/ableton_remover.bat
@@ -0,0 +1,9 @@
+
+REM option file location "C:\ProgramData\Ableton\CommonConfiguration\Live 11.1.1\Preferences\Options"
+REM user pref location "C:\Users\%username%\AppData\Roaming\Ableton\Live 11.1.1\Preferences"
+
+
+REM Need to loop through all Ableton versions and remove license line
+
+REM need to loop thought preferences possible on user account
+

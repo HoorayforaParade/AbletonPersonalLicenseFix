f94fa6e8b023dabf2406b2337cc946eca6cf4625
Neil Andruski <neil.andruski@gmail.com>
2023-01-02 13:03:15 -0500

    Update option_editor.sh
    updated echo chooses, Added instructions for script.
    
    Added a TODO for Usage and option input to skip selection.

--- MacOS/option_editor.sh
+++ MacOS/option_editor.sh
@@ -6,13 +6,23 @@
 # Directory path to Ableton preferences folder. Where each version has their own directory
 path="/Users/$USER/Library/Preferences/Ableton"
 
-# Allow user to select the level of deletion of the licenser server option.
+#TODO: Add Usage.
 
-echo "Please enter your level of deletion"
+#TODO: Add option at run and skip choice selection.
+
+# Allow user to select the level of deletion of the licenser server option.
+echo "
+Please enter your level of deletion:
+	1) Remove option file but keep backup
+	2) Remove license server option in files
+	3) Choose lines to remove from files
+
+Which option do you choose:"
 
 read choice
 
-echo choice
+#For testing.
+#echo choice
 
 # Loop through the Ableton Live version folders. Find the Options file and replaces the license server line.
 for dir in "$path"/* ; do
@@ -20,13 +30,13 @@ for dir in "$path"/* ; do
 	
 	# Rename file so license server option doesn't run
 	if [["$choice" == "1"]]; then
-		echo 3
+		echo 1
 		#mv "$dir"/Options.txt "$dir"/Options.txt.bak
 	fi
 	
 	# Change the license server line, Save original with .bak postfix to roll back if user needs to.
 	if [["$choice" == "2"]]; then
-		echo 3
+		echo 2
 		#sed -i '.bak' -r -e 's/-[Ll]icenseserver//g' "$dir"/Options.txt
 	fi
 	
